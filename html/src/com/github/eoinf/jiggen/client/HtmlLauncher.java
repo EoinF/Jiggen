42e089c12db1f27b1da435a5b4698989490cbf0d
Eoin Flanagan <outterest@hotmail.com>
2018-05-16 00:49:30 +0200

    Added gwt integration with react project React project now builds along with the html module

--- html/src/com/github/eoinf/jiggen/client/HtmlLauncher.java
+++ html/src/com/github/eoinf/jiggen/client/HtmlLauncher.java
@@ -4,16 +4,37 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import com.github.eoinf.jiggen.Jiggen;
+import com.google.gwt.core.client.GWT;
+import jsinterop.annotations.JsMethod;
+import jsinterop.annotations.JsPackage;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
-        public GwtApplicationConfiguration getConfig () {
+        public GwtApplicationConfiguration getConfig() {
                 return new GwtApplicationConfiguration(800, 600);
         }
 
         @Override
-        public ApplicationListener createApplicationListener () {
+        public String getPreloaderBaseURL()
+        {
+                String url = getHostPageBaseURL() + "assets/";
+                GWT.log(url);
+                return url;
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener() {
                 return new Jiggen();
         }
+
+        @JsMethod(namespace = JsPackage.GLOBAL)
+        public static native void renderReact(String containerId) /*-{
+                window.renderReact(containerId)
+}-*/;
+
+        public static native String getHostPageBaseURL() /*-{
+            var s = $doc.location.origin;
+            return s + "/" + $moduleName + "/";
+          }-*/;
 }
