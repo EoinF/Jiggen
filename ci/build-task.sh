c0cb186e99abe7cc4f9ee92178d2e64b921cf64b
Eoin Flanagan <outterest@hotmail.com>
2019-03-31 21:33:19 +0200

    Added debugging line to build-task.sh to allow ssh before the container is destroyed * Removed unnecessary routes from sw.js * Added robots.txt

--- ci/build-task.sh
+++ ci/build-task.sh
@@ -5,5 +5,19 @@ cd ../html/react
 npm install -q
 cd ../..
 ./gradlew html:dist -q
+gzip -r html/build/dist
 
-aws s3 cp html/build/dist s3://cdn.jiggen.app/ --recursive
+tail -f /dev/null # Wait forever
+
+#aws s3 cp html/build/dist s3://cdn.jiggen.app/ --recursive \
+#--include "*" --acl "public-read" \
+#--exclude "*.js" \
+#--exclude "*.css" \
+#--delete
+#
+#
+#aws s3 cp html/build/dist s3://cdn.jiggen.app/ --recursive \
+#--exclude "*" \
+#--include "*.js" --acl "public-read" --content-encoding gzip \
+#--include "*.css" --acl "public-read" --content-encoding gzip \
+#--delete
