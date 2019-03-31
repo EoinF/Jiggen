f06760782710441a4a9040a8c48439cf96133993
Eoin Flanagan <outterest@hotmail.com>
2019-03-31 23:23:38 +0200

    Modified dist task to run service worker build script Modified build-task.sh to manually gzip and upload specific file types to s3

--- ci/build-task.sh
+++ ci/build-task.sh
@@ -5,19 +5,43 @@ cd ../html/react
 npm install -q
 cd ../..
 ./gradlew html:dist -q
-gzip -r html/build/dist
 
-tail -f /dev/null # Wait forever
+find html/build/dist -type f \
+    ! -name '*.gz' \
+    ! -name '*.png' \
+    ! -name '*.ico' \
+    ! -name '*.swf' \
+        -exec gzip "{}" \;
 
-#aws s3 cp html/build/dist s3://cdn.jiggen.app/ --recursive \
-#--include "*" --acl "public-read" \
-#--exclude "*.js" \
-#--exclude "*.css" \
-#--delete
-#
-#
-#aws s3 cp html/build/dist s3://cdn.jiggen.app/ --recursive \
-#--exclude "*" \
-#--include "*.js" --acl "public-read" --content-encoding gzip \
-#--include "*.css" --acl "public-read" --content-encoding gzip \
-#--delete
+for f in `find html/build/dist -type f -name '*.gz'`; do
+    mv $f ${f%.gz}
+done
+
+aws s3 sync html/build/dist s3://cdn.jiggen.app/ \
+--include "*" --acl "public-read" \
+--exclude "*.js" \
+--exclude "*.css" \
+--exclude "*.html" \
+--exclude "*.fnt" \
+--exclude "*.atlas" \
+--exclude "*.txt" \
+--exclude "*.glsl" \
+--exclude "*.frag" \
+--exclude "*.vert" \
+--exclude "*.json" \
+--exclude "*.map" \
+--delete
+
+aws s3 sync html/build/dist s3://cdn.jiggen.app/ \
+--exclude "*" \
+--include "*.js" --acl "public-read" --content-encoding gzip \
+--include "*.css" --acl "public-read" --content-encoding gzip \
+--include "*.html" --acl "public-read" --content-encoding gzip \
+--include "*.fnt" --acl "public-read" --content-encoding gzip \
+--include "*.atlas" --acl "public-read" --content-encoding gzip \
+--include "*.txt" --acl "public-read" --content-encoding gzip \
+--include "*.glsl" --acl "public-read" --content-encoding gzip \
+--include "*.frag" --acl "public-read" --content-encoding gzip \
+--include "*.vert" --acl "public-read" --content-encoding gzip \
+--include "*.json" --acl "public-read" --content-encoding gzip \
+--delete
