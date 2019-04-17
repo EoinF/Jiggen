df2d8aa8c7e73ea96edd3b1f4b96b1fa9a84e921
Eoin Flanagan <outterest@hotmail.com>
2019-04-17 20:42:18 +0200

    Now correctly calculates the width, height and position of the connect puzzle pieces

--- core/src/com/github/eoinf/jiggen/webapp/screens/models/ConnectedPuzzlePieces.java
+++ core/src/com/github/eoinf/jiggen/webapp/screens/models/ConnectedPuzzlePieces.java
@@ -7,22 +7,33 @@ import java.util.List;
 
 public class ConnectedPuzzlePieces {
     private List<PuzzlePiece> connectedPieces;
+
     private float width;
     private float height;
+    private Vector2 position;
 
     public ConnectedPuzzlePieces(PuzzlePiece piece) {
-        this.connectedPieces = new ArrayList<>();
-        this.connectedPieces.add(piece);
-
-        this.width = piece.getWidth();
-        this.height = piece.getHeight();
+        List<PuzzlePiece> connectedPieces = new ArrayList<>();
+        connectedPieces.add(piece);
+        this.setConnectedPieces(connectedPieces);
     }
 
     public void setConnectedPieces(List<PuzzlePiece> connectedPieces) {
         this.connectedPieces = connectedPieces;
-        for (PuzzlePiece piece: connectedPieces) {
+        float left = Float.MAX_VALUE;
+        float right = Float.MIN_VALUE;
+        float top = Float.MAX_VALUE;
+        float bottom = Float.MIN_VALUE;
 
+        for (PuzzlePiece piece: connectedPieces) {
+            left = Math.min(left, piece.x());
+            right = Math.max(right, piece.x() + piece.getWidth());
+            top = Math.min(top, piece.y());
+            bottom = Math.max(bottom, piece.y() + piece.getHeight());
         }
+        this.width = right - left;
+        this.height = bottom - top;
+        this.position = new Vector2(left, top);
     }
 
     public List<PuzzlePiece> getConnectedPieces() {
@@ -38,12 +49,14 @@ public class ConnectedPuzzlePieces {
     }
 
     public Vector2 getPosition() {
-        return connectedPieces.get(0).getPosition();
+        return position;
     }
 
     public void setPosition(Vector2 nextPosition) {
         // Get the offset from the base position
         nextPosition.sub(getPosition());
+
+        this.position.add(nextPosition);
 
         for (PuzzlePiece puzzlePiece: connectedPieces) {
             puzzlePiece.setPosition(puzzlePiece.getPosition().add(nextPosition));
